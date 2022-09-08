7687113e9ff346e8123c17c036d453c8be3b5620
philt3r <philip.beadle@live.com.au>
2022-09-08 12:43:27 +1000

    IOEN Protocol recording ecogrid and retail transactions

--- ioen-protocol-socket/server.js
+++ ioen-protocol-socket/server.js
@@ -20,6 +20,8 @@ const TIMEOUT = 30000;
 let adminClient = {};
 let appClient = {};
 let adminPort = 65000;
+const ecoGridTransactionsToProcess = [];
+const retailTransactionsToProcess = [];
 async function connectToHolochain() {
     adminClient = await AdminWebsocket.connect(`ws://localhost:${adminPort}`, TIMEOUT);
     const appPort = await adminClient.attachAppInterface({ port: 0 });
@@ -28,103 +30,149 @@ async function connectToHolochain() {
 
 connectToHolochain();
 
-async function createNewAgent(socket, nanoGrid) {
+async function createNewAgent(socket, installed_app_id, callback) {
+    const agent_key = await adminClient.generateAgentPubKey();
+    console.log(agent_key);
+    const base64AgentPubKey = base64.bytesToBase64(agent_key);
+    const agent = {
+        agentPubKey: base64AgentPubKey
+    }
+    console.log(agent);
+    socket.emit('AgentPubKeyGenerated', agent);
+    const path = '/Users/philipbeadle/IOEN/Protocol/ioen-protocol/workdir/ioen-protocol.happ';
+    const installedApp = await adminClient.installAppBundle({
+        path,
+        agent_key,
+        installed_app_id,
+        membrane_proofs: {},
+    });
+    console.log(installedApp);
+    await adminClient.enableApp({ installed_app_id });
+    const nano_grid_settings_cell_id = installedApp.cell_data.find(data => data.role_id === 'nanogrid').cell_id;
+    const nanoGridSettingsCellId = base64.bytesToBase64(nano_grid_settings_cell_id[0]);
+    const transactions_cell_id = installedApp.cell_data.find(data => data.role_id === 'energy').cell_id;
+    const transactionsCellId = base64.bytesToBase64(transactions_cell_id[0]);
+    const billing_cell_id = installedApp.cell_data.find(data => data.role_id === 'billing').cell_id;
+    const billingCellId = base64.bytesToBase64(billing_cell_id[0]);
+    const ioenFuelCellId = "ioenFuelCellId";
+    const protocolAppInfo = {
+        installedAppId: installed_app_id,
+        nanoGridSettingsCellId,
+        ioenFuelCellId,
+        billingCellId,
+        transactionsCellId
+    }
+    console.log(protocolAppInfo);
+    socket.emit('AppBundleInstalled', protocolAppInfo);
+    callback(agent_key, protocolAppInfo)
+}
+
+function createNewNanoGrid(socket, nanoGrid) {
     try {
         console.log(nanoGrid);
-        const nanoGridName = nanoGrid.nanoGridName;
-        const agent_key = await adminClient.generateAgentPubKey();
-        console.log(agent_key);
-        const base64AgentPubKey = base64.bytesToBase64(agent_key);
-        const agent = {
-            nanoGridName,
-            agentPubKey: base64AgentPubKey
-        }
-        console.log(agent);
-        socket.emit('AgentPubKeyGenerated', agent);
-        const installed_app_id = nanoGridName;
-        const path = '/Users/philipbeadle/IOEN/Protocol/ioen-protocol/workdir/ioen-protocol.happ';
-        const installedApp = await adminClient.installAppBundle({
-            path,
-            agent_key,
-            installed_app_id,
-            membrane_proofs: {},
-        });
-        console.log(installedApp);
-        await adminClient.enableApp({ installed_app_id });
-        const nano_grid_settings_cell_id = installedApp.cell_data.find(data => data.role_id === 'nanogrid').cell_id;
-        const nanoGridSettingsCellId = base64.bytesToBase64(nano_grid_settings_cell_id[0]);
-        const transactions_cell_id = installedApp.cell_data.find(data => data.role_id === 'energy').cell_id;
-        const transactionsCellId = base64.bytesToBase64(transactions_cell_id[0]);
-        const billing_cell_id = installedApp.cell_data.find(data => data.role_id === 'billing').cell_id;
-        const billingCellId = base64.bytesToBase64(billing_cell_id[0]);
-        const ioenFuelCellId = "ioenFuelCellId";
-        const protocolAppInfo = {
-            installedAppId: nanoGridName,
-            nanoGridSettingsCellId,
-            ioenFuelCellId,
-            billingCellId,
-            transactionsCellId
-        }
-        console.log(protocolAppInfo);
-
-        socket.emit('AppBundleInstalled', protocolAppInfo);
-        const actionHash = await appClient.callZome({
-            cap_secret: null,
-            cell_id: nano_grid_settings_cell_id,
-            zome_name: 'settings',
-            fn_name: 'create_nanogrid',
-            payload: nanoGrid,
-            provenance: nano_grid_settings_cell_id[1]
-        });
-        console.log(base64.bytesToBase64(actionHash));
-        socket.emit('NanoGridCreated', base64.bytesToBase64(actionHash));
+        createNewAgent(socket, nanoGrid.nanoGridName, (agent_key, protocolAppInfo) => {
+            const cell_id = [base64.base64ToBytes(protocolAppInfo.nanoGridSettingsCellId), agent_key];
+            appClient.callZome({
+                cap_secret: null,
+                cell_id,
+                zome_name: 'settings',
+                fn_name: 'create_nanogrid',
+                payload: nanoGrid,
+                provenance: agent_key
+            })
+            .then(actionHash => {
+                console.log(base64.bytesToBase64(actionHash));
+                socket.emit('NanoGridCreated', base64.bytesToBase64(actionHash));
+            });
+        })
     } catch (error) {
         console.error(error);
     }
-   
 };
 
-async function createEcoGridTransaction(agentPubKey, transactionsCellId, ecoGridTransaction) {
+let ecoGridTransactionInProgress = false;
+async function createEcoGridTransaction() {
+    if (ecoGridTransactionInProgress == true) return;
+    const payload = ecoGridTransactionsToProcess.shift();
+    if (payload == undefined) return;
+    const start = new Date();
     try {
-        const agent_key = base64.base64ToBytes(agentPubKey);
-        const transactions_cell_id = [base64.base64ToBytes(transactionsCellId), agent_key];
-        ecoGridTransaction.consumerNanoGrid = base64.base64ToBytes(ecoGridTransaction.consumerNanoGrid);
-        ecoGridTransaction.supplierNanoGrid = base64.base64ToBytes(ecoGridTransaction.supplierNanoGrid);
-        const actionHash = await appClient.callZome({
+        ecoGridTransactionInProgress = true;
+        const cell_id = [base64.base64ToBytes(payload.transactionsCellId), base64.base64ToBytes(payload.consumerNanoGrid)];
+        payload.consumerNanoGrid = base64.base64ToBytes(payload.consumerNanoGrid);
+        payload.supplierNanoGrid = base64.base64ToBytes(payload.supplierNanoGrid);
+        delete payload.transactionsCellId;
+
+        await appClient.callZome({
             cap_secret: null,
-            cell_id: transactions_cell_id,
+            cell_id,
             zome_name: 'transactions',
             fn_name: 'create_eco_grid_transaction',
-            payload: ecoGridTransaction,
-            provenance: agent_key
+            payload,
+            provenance: cell_id[1]
         });
-        console.log(base64.bytesToBase64(actionHash));
+        const end = new Date();
+        console.log("Elapsed time createEcoGridTransaction " + (end.getTime() - start.getTime()) + " ms - left to process " + ecoGridTransactionsToProcess.length);
+        ecoGridTransactionInProgress = false;
     } catch (error) {
+        ecoGridTransactionInProgress = false;
         console.error(error);
     }
 }
+
+setInterval(createEcoGridTransaction, 10);
+
+let retailTransactionInProgress = false;
+async function createRetailTransaction() {
+    if (retailTransactionInProgress == true) return;
+    const payload = retailTransactionsToProcess.shift();
+    if (payload == undefined) return;
+    const start = new Date();
+    try {
+        retailTransactionInProgress = true;
+        const cell_id = [base64.base64ToBytes(payload.transactionsCellId), base64.base64ToBytes(payload.consumerNanoGrid)];
+        payload.consumerNanoGrid = base64.base64ToBytes(payload.consumerNanoGrid);
+        delete payload.transactionsCellId;
+
+        await appClient.callZome({
+            cap_secret: null,
+            cell_id,
+            zome_name: 'transactions',
+            fn_name: 'create_retail_transaction',
+            payload,
+            provenance: cell_id[1]
+        });
+        const end = new Date();
+        console.log("Elapsed time createRetailTransaction " + (end.getTime() - start.getTime()) + " ms - left to process " + retailTransactionsToProcess.length);
+        retailTransactionInProgress = false;
+    } catch (error) {
+        retailTransactionInProgress = false;
+        console.error(error);
+    }
+}
+
+setInterval(createRetailTransaction, 10);
+
 io.on('connection', (socket) => {
     var cnt = 0;
-    console.log('[' + (new Date()).toUTCString() + '] unity connecting with SocketID ' + socket.id);	
-    
-    socket.on('PING', (data) => {
-        cnt++;
-        console.log('[' + (new Date()).toUTCString() + '] incoming PING #' + data + ' answering PONG with some jitter...');
-        setTimeout(() => {
-            socket.emit('PONG', data);
-        }, 1000)
+    console.log('[' + (new Date()).toUTCString() + '] IOEN World connecting with NanoGridName ' + socket.handshake.auth.id);	
+
+    socket.on('CreateNewRetailer', () => {        
+        createNewAgent(socket, 'Redgrid Energy' , (agent_key, protocolAppInfo) => {
+            console.log('CreateNewRetailer' + agent_key);
+        });
     });
 
-    socket.on('CreateNewAgent', (nanoGrid) => {        
-        createNewAgent(socket, nanoGrid);
+    socket.on('CreateNewNanoGrid', (nanoGrid) => {        
+        createNewNanoGrid(socket, nanoGrid);
     });
 
     socket.on('CreateEcoGridTransaction', (transaction) => {
-        console.log("payload", transaction);
-        const agentPubKey = transaction.supplierNanoGrid;
-        const transactionsCellId = transaction.transactionsCellId;
-        delete transaction.transactionsCellId;
-        createEcoGridTransaction(agentPubKey, transactionsCellId, transaction);
+        ecoGridTransactionsToProcess.push(transaction);
+    });
+
+    socket.on('CreateRetailTransaction', (transaction) => {
+        retailTransactionsToProcess.push(transaction);
     });
 
     socket.on('disconnect', (reason) => {
