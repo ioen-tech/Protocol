8db4f22d072a0f04b7ad3b168cfe1e06df7a59db
philt3r <philip.beadle@live.com.au>
2022-09-15 10:51:26 +1000

    new project for Tesla Gateway client

---
+++ tesla-gateway-client/index.js
@@ -0,0 +1,99 @@
+const io = require("socket.io-client");
+const fs = require('fs');
+const socket = io("http://localhost:8080", {
+  auth: {
+    id: "Tesla Gateway"
+  }
+});
+const agentPubKeyFileName = '/Users/philipbeadle/IOEN/Protocol/tesla-gateway-client/agentPubKey2.txt'
+const url = 'https://192.168.0.185/api/meters/aggregates'
+const port = process.argv[2]
+const email = 'philip.beadle@live.com.au'
+const password = 'IoenRocks!'
+let agentPubKey = '';
+let protocolAppInfo = '';
+const axios = require('axios')
+const https = require('https')
+let token = ''
+axios
+  .post('https://192.168.0.185/api/login/Basic', {
+    email,
+    password,
+    username: 'customer'
+  },
+  {
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false
+    }),
+    headers: { Authorization: `Bearer ${token}` }
+  })
+  .then(res => {
+    console.log(`statusCode: ${res.status}`)
+    token = res.data.token
+    console.log(token)
+  })
+  .catch(error => {
+    console.error(error)
+  });
+
+let availableEnergy = 0;
+function calculatePowerRequirements() {
+  if (availableEnergy > 0) {
+    // Any left over energy is used to charge the local community battery
+  }
+  axios
+    .get('https://192.168.0.185/api/meters/aggregates', 
+    {
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false
+      }),
+      headers: { Authorization: `Bearer ${token}` }
+    })
+    .then(res => {
+      const currentTime = new Date()
+      const hour = currentTime.getHours() + 1
+      const day = currentTime.getDate()
+      const month = currentTime.getMonth() + 1
+      const year = currentTime.getFullYear()
+  
+      // Tesla Gateway value for site.instant_power is the amount either being bought from the retailer or sold as Feed In Tariff
+      // hence its the amount of available energy that can be sold to other nano grids via a supply agreement.
+      availableEnergy = res.data.site.instant_power;
+    });
+}
+
+let isCreatingNewNanoGrid = false;
+socket.on("connect", () => {
+  console.log(socket.id);
+  try {
+    agentPubKey = fs.readFileSync(agentPubKeyFileName);
+    console.log('agentPubkey ' + agentPubKey)
+  } catch (err) {
+    console.log(err)
+  }
+  if (agentPubKey == '' && isCreatingNewNanoGrid == false) {
+    console.log('first time agent')
+    const nanogrid = {
+        nanoGridName: '58 Alice St',
+        numberOfSolarPanels: 32,
+        storageCapacity: 13
+    };
+    isCreatingNewNanoGrid = true;
+    socket.emit('CreateNewNanoGrid', nanogrid)
+  }
+
+  socket.on('AgentPubKeyGenerated', agent => {
+    agentPubKey = agent.agentPubKey;
+    console.log('AgentPubKeyGenerated ' + JSON.stringify(agent));
+    console.log('AgentPubKey ' + agentPubKey);
+    try {
+        fs.writeFileSync(agentPubKeyFileName, agentPubKey, {encoding:'utf8',flag:'w'});
+    } catch (err) {
+        console.log('err' + err)
+    }
+    isCreatingNewNanoGrid = false;
+  });
+});
+    
+
+// fs.writeFileSync('/tmp/test-sync', 'Hey there!');
